a4ddbc03db90b661d4c1a1bd69937ff4e59fd7cb
joshua hammer <jph33@outlook.com>
2025-05-06 09:35:12 -0400

    homefully static fixes it

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported, or if you want to use a specific adapter like adapter-node or adapter-static,
-    // you can specify it here.
-    adapter: adapter()
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false
+    }),
+    prerender: {
+      entries: ['*', '/api/events', '/api/groups']
+    }
   }
 };
-
-export default config;
-
